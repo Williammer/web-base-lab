d31baf55e98be4165d806aac0da63415640d62a0
Williammer <supervegeter@gmail.com>
2017-09-20 08:18:14 +0800

    clean

--- sandbox/sandbox.js
+++ sandbox/sandbox.js
@@ -1,30 +1,30 @@
 const proxyCache = new WeakMap()
 
 function compileCode(src) {
-    // 1. create a reusable sandbox function env.
-    // 2. use with to protect the global access(can't hv 'use strict')
-    const code = new Function("sandbox", `with(sandbox){ ${src} }`)
+  // 1. create a reusable sandbox function env.
+  // 2. use with to protect the global access(can't hv 'use strict')
+  const code = new Function("sandbox", `with(sandbox){ ${src} }`)
 
-    return function(sandbox) {
-        // 5. use WeakMap as cache for proxy
-        if (!proxyCache.has(sandbox)) {
-            const sandboxProxy = new Proxy(sandbox, { has, get })
-            proxyCache.set(sandbox, sandboxProxy)
-        }
-
-        return code(proxyCache.get(sandbox))
+  return function(sandbox) {
+    // 5. use WeakMap as cache for proxy
+    if (!proxyCache.has(sandbox)) {
+      const sandboxProxy = new Proxy(sandbox, { has, get })
+      proxyCache.set(sandbox, sandboxProxy)
     }
+
+    return code(proxyCache.get(sandbox))
+  }
 }
 
 // 3. this trap intercepts 'in' operations on sandboxProxy to prevent the 'in' operator in 'with'
 function has(target, key) {
-    return true
+  return true
 }
 
 function get(target, key, receiver) {
-    // 4. The Symbol.unscopables is used to specify an object value of whose own and inherited property names are excluded from the 'with' environment bindings of the associated object.
-    if (key === Symbol.unscopables) return undefined
-    return Reflect.get(target, key, receiver)
+  // 4. The Symbol.unscopables is used to specify an object value of whose own and inherited property names are excluded from the 'with' environment bindings of the associated object.
+  if (key === Symbol.unscopables) return undefined
+  return Reflect.get(target, key, receiver)
 }
 
 /**
--- template-simple/main.js
+++ template-simple/main.js
@@ -1,13 +1,13 @@
 var colorsArray = ['red', 'green', 'blue', 'orange'];
 var items = [];
 for (var i = 0; i < 10000; i++) {
-    items.push("test");
+  items.push("test");
 }
 
 function replaceContent(name) {
-    document.getElementById('content').innerHTML =
-        tmpl(name, {
-            colors: colorsArray,
-            items: items
-        });
+  document.getElementById('content').innerHTML =
+    tmpl(name, {
+      colors: colorsArray,
+      items: items
+    });
 }
