604fe0a6409e0cb26c3d180170dfa248c254455d
Williammer <supervegeter@gmail.com>
2018-04-21 21:06:26 +0800

    finish the data->view notify

--- databinding/my-defprop.js
+++ databinding/my-defprop.js
@@ -1,40 +1,71 @@
-
-
-
-
-
-// const vmCore = 
-
-
-
-
-
-
 export default class VM {
   constructor(context) {
-    const blockElement = document.querySelector("#scope-block");
+    const parent = document.querySelector(context.el);
+    this._watchers = [];
     this._data = context.data;
-    this._initObserver()
-  }
 
-  _initObserver() {
-    const initVal = this._data.prop;
+    this._initObservers();
+    this._parseView(parent);
+  }
+  _parseView(parent) {
+    const children = parent.children;
+
+    // Iterate array-like HTMLCollection
+    Reflect.apply(Array.prototype.forEach, children, [node => {
+      const bindedData = node.getAttribute("bind");
+      const modelledData = node.getAttribute("model");
+      if (modelledData) {
+        this._watchers.push({ [modelledData]: this._modelHandler.bind(this, node) });
+      } else if (bindedData) {
+        this._watchers.push({ [bindedData]: this._bindHandler.bind(this, node) });
+      }
+    }]);
+
+    console.log("this._watchers: ", this._watchers);
+  }
+  _initObservers() {
     const propNames = Object.keys(this._data);
-    propNames.forEach((prop) => {
-      Object.defineProperty(this._data, prop, {
-        // writable: true,
-        configurable: true,
-        enumerable: true,
-        // value: initVal,
-        get() {
-          return this._data[prop];
-        },
-        set(value) {
-          this._data[prop] = value;
-          // notify watchers
-          
-        }
+    this._initObserver = this._initObserver.bind(this);
+    propNames.forEach(this._initObserver);
+  }
+  _initObserver(propName) {
+    // use cached value to avoid infinite loop of get/set
+    let value = this._data[propName];
+    const notify = this._notify.bind(this);
+
+    Object.defineProperty(this._data, propName, {
+      configurable: true,
+      enumerable: true,
+      get() {
+        // the `this` scope within this descriptor is the target data
+        return value;
+      },
+      set(newValue) {
+        value = newValue;
+
+        notify(propName);
       }
     });
+  }
+  _notify(propName) {
+    this._watchers.forEach((watcher) => {
+      watcher[propName](propName); // invoke the handler
+    })
+  }
+
+  _bindHandler(node, propName) {
+    console.log("called _bindHandler");
+    // handle how to update the `bind` elements
+    node.textContent = this._data[propName];
+  }
+
+  _modelHandler(node, propName) {
+    console.log("called _modelHandler");
+    // handle how to update the `bind` elements
+    node.value = this._data[propName];
+  }
+
+  get data() {
+    return this._data;
   }
 }
