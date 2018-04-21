9a1a141987889ed87b4b5ec0160a766c6948061e
Williammer <supervegeter@gmail.com>
2018-04-21 23:19:59 +0800

    Updated to 2-way

--- databinding/my-defprop.js
+++ databinding/my-defprop.js
@@ -1,70 +1,66 @@
 export default class VM {
   constructor(context) {
-    const parent = document.querySelector(context.el);
+    this._root = document.querySelector(context.el);
     this._watchers = [];
     this._data = context.data;
 
-    this._initObservers();
-    this._parseView(parent);
+    this._observe(this._data);
+    this._parseView(this._root);
   }
-  _parseView(parent) {
-    const children = parent.children;
+  _parseView(root) {
+    const children = root.children;
 
     // Iterate array-like HTMLCollection
     Reflect.apply(Array.prototype.forEach, children, [node => {
-      const bindedData = node.getAttribute("bind");
-      const modelledData = node.getAttribute("model");
-      if (modelledData) {
-        this._watchers.push({ [modelledData]: this._modelHandler.bind(this, node) });
-      } else if (bindedData) {
-        this._watchers.push({ [bindedData]: this._bindHandler.bind(this, node) });
+      const bindDataName = node.getAttribute("bind");
+      const modelDataName = node.getAttribute("model");
+      if (modelDataName) {
+        this._watchers.push({ [modelDataName]: this._modelHandler.bind(this, node) });
+
+        node.addEventListener("input", ({ target: { value } }) => {
+          this._data[modelDataName] = value;
+        });
+      } else if (bindDataName) {
+        this._watchers.push({ [bindDataName]: this._bindHandler.bind(this, node) });
       }
     }]);
-
-    console.log("this._watchers: ", this._watchers);
   }
-  _initObservers() {
-    const propNames = Object.keys(this._data);
-    this._initObserver = this._initObserver.bind(this);
-    propNames.forEach(this._initObserver);
-  }
-  _initObserver(propName) {
-    // use cached value to avoid infinite loop of get/set
-    let value = this._data[propName];
+  _observe(data) {
     const notify = this._notify.bind(this);
 
-    Object.defineProperty(this._data, propName, {
-      configurable: true,
-      enumerable: true,
-      get() {
-        // the `this` scope within this descriptor is the target data
-        return value;
-      },
-      set(newValue) {
-        value = newValue;
+    Object.keys(data).forEach((propName) => {
+      // use cached value to avoid infinite loop of get/set
+      let value = data[propName];
 
-        notify(propName);
+      if (typeof value === "object") {
+        this._observe(value);
       }
+
+      Object.defineProperty(data, propName, {
+        configurable: true,
+        enumerable: true,
+        get() {
+          // the `this` scope within this descriptor is the target data
+          return value;
+        },
+        set(newValue) {
+          value = newValue;
+          notify(propName); // TODO: change it to have each Watcher
+        }
+      });
     });
   }
   _notify(propName) {
     this._watchers.forEach((watcher) => {
-      watcher[propName](propName); // invoke the handler
+      watcher[propName](propName); // invoke the mutation handler
     })
   }
-
   _bindHandler(node, propName) {
-    console.log("called _bindHandler");
-    // handle how to update the `bind` elements
     node.textContent = this._data[propName];
   }
-
   _modelHandler(node, propName) {
-    console.log("called _modelHandler");
-    // handle how to update the `bind` elements
     node.value = this._data[propName];
   }
-
   get data() {
     return this._data;
   }
