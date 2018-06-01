f19348885f073c4262708d72923e107ef3274aaa
Williammer <supervegeter@gmail.com>
2018-06-01 08:31:18 +0800

    tried my best

--- my-promise/promise.js
+++ my-promise/promise.js
@@ -1,48 +1,103 @@
-function fulfill(data) {
+const PENDING = Symbol.for("pending");
+const RESOLVED = Symbol.for("resolved");
+const REJECTED = Symbol.for("rejected");
+
+function isFunction(val) {
+  return typeof val === "function";
+}
+function isPending(promise) {
+  return promise["[[PromiseStatus]]"] === PENDING;
+}
+function isFulfilled(promise) {
+  return promise["[[PromiseStatus]]"] === RESOLVED;
+}
+function isRejected(promise) {
+  return promise["[[PromiseStatus]]"] === REJECTED;
+}
+
+function nextTick(fn) {
+  const tick = setTimeout(() => {
+    fn();
+    clearTimeout(tick);
+  }, 0);
+}
+
+
+/* these 2 functions are the key processor */
+function fulfill_(data) {
   this["[[PromiseValue]]"] = data;
-  this["[[PromiseStatus]]"] = "resolved";
+  this["[[PromiseStatus]]"] = RESOLVED;
+
+  this._handlers.reduce((accu, handler, index) => {
+    if (handler.type !== RESOLVED) {
+      return accu;
+    }
+
+    // make async?
+    return handler(accu);
+  }, data);
 }
-function reject(error) {
+/* these 2 functions are the key processor */
+function reject_(error) {
   this["[[PromiseValue]]"] = error;
-  this["[[PromiseStatus]]"] = "rejected";
+  this["[[PromiseStatus]]"] = REJECTED;
+
+  this._handlers.reduce((accu, handler, index) => {
+    if (handler.type !== REJECTED) {
+      return accu;
+    }
+
+    // make async?
+    return handler(accu);
+  }, error);
 }
+
 
 class MyPromise {
   static resolve(data) {
     const promise = new this();
-    fulfill.call(promise, data);
+    fulfill_.call(promise, data);
     return promise;
   }
 
   static reject(error) {
     const promise = new this();
-    reject.call(promise, error);
+    reject_.call(promise, error);
     return promise;
   }
 
   constructor(executor) {
-    this["[[PromiseStatus]]"] = "pending";
+    this["[[PromiseStatus]]"] = PENDING;
+    this._handlers = [];
 
-    if (typeof executor === "function") {
+    if (isFunction(executor)) {
       try {
-        executor(fulfill.bind(this), reject.bind(this));
+        executor(fulfill_.bind(this), reject_.bind(this));
       } catch(e) {
-        reject.call(this, e);
+        reject_.call(this, e);
       }
     }
   }
 
-  then(onFufilled, onRejected) {
-    setTimeout(() => {
-      typeof onFufilled === "function" && onFufilled(this["[[PromiseValue]]"]);
-    }, 0);
+  // use as the register function
+  then(onFulfilled, onRejected) {
+    if (isFunction(onFulfilled)) {
+      onFulfilled.type = RESOLVED;
+      this._handlers.push(onFulfilled);
+    }
+    if (isFunction(onRejected)) {
+      onFulfilled.type = REJECTED;
+      this._handlers.push(onRejected);
+    }
+
     return this;
   }
 
   catch(onRejected) {
-    setTimeout(() => {
-      typeof onRejected === "function" && onRejected(this["[[PromiseValue]]"]);
-    }, 0);
+    if (isFunction(onRejected)) {
+      this._handlers.push({ REJECTED: onRejected });
+    }
+
     return this;
   }
 }
