95ba61466ecacaed2eab684fefcd145a66d5d8d3
Williammer <supervegeter@gmail.com>
2018-07-22 19:20:02 +0800

    re-organized the experiments to use same babel/test/lint

---
+++ try-workerize/greenlet.test.js
@@ -0,0 +1,25 @@
+import greenlet from './greenlet';
+
+describe.skip('greenlet', () => {
+	it('should return an async function', () => {
+		let g = greenlet( () => 'one' );
+		expect(g).toEqual(jasmine.any(Function));
+		expect(g()).toEqual(jasmine.any(Promise));
+	});
+
+	it('should invoke sync functions', async () => {
+		let foo = greenlet( a => 'foo: '+a );
+
+		let ret = await foo('test');
+		expect(ret).toEqual('foo: test');
+	});
+
+	it('should invoke async functions', async () => {
+		let bar = greenlet( a => new Promise( resolve => {
+			resolve('bar: '+a);
+		}));
+
+		let ret = await bar('test');
+		expect(ret).toEqual('bar: test');
+	});
+});
